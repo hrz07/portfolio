95ea5840007c13f1cf7ff4c02337b9d24e0dc287
hrz07 <hridoy.developer@yahoo.com>
2022-06-12 12:16:10 +0600

    logo updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,6 +10,7 @@ import {
 import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import Logo from '../assets/logo.png';
+import Hridoy from '../assets/hridoy.png';
 import { Link } from 'react-scroll';
 import  cv from '../assets/Hridoy_resume.pdf'
 
@@ -21,7 +22,7 @@ const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
       <div>
-        <img src={Logo} alt='Logo Image' style={{ width: '50px' }} />
+        <img src={Hridoy} alt='Logo Image' style={{ width: '200px' }} />
       </div>
 
       {/* menu */}
